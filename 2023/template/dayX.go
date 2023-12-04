2171821e9748d08626d05fba2d889e186c16c708
Bart Westenenk <bart.westenenk@gmail.com>
2023-12-04 10:16:00 +0100

    Remove old format

---
+++ 2023/template/dayX.go
@@ -0,0 +1,18 @@
+package main
+
+func GetDayX() [2]func(input string) int {
+	return [2]func(input string) int{
+		challengeXa,
+		challengeXb,
+	}
+}
+
+func challengeXa(input string) int {
+	answer := 0
+	return answer
+}
+
+func challengeXb(input string) int {
+	answer := 0
+	return answer
+}
