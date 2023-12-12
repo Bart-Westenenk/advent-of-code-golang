00bd7bd4c8c7ba1b8e76d4f3961118edead0ff9f
Bart Westenenk <bart@westenenk.me>
2023-12-12 22:40:11 +0100

    Day 9

--- 2023/solutions/day9.go
+++ 2023/solutions/day9.go
@@ -35,40 +35,47 @@ func extrapolate(deltaArray []int) int {
 		return 0
 	}
 
-	for i := 0; i < len(deltaArray)-1; i++ {
-		delta := deltaArray[i+1] - deltaArray[i]
-		newDelta = append(newDelta, delta)
-	}
-
-	// Otherwise, keep exterpolating
-	return newDelta[len(newDelta)-1] + extrapolate(newDelta)
-}
-
-func extrapolateBackwards(deltaArray []int) int {
-	var newDelta []int
-	allZero := slices.Max(deltaArray) == slices.Min(deltaArray)
-	// If input exists of only zeroes, exterpolation will return zero [BASE CASE]
-	if allZero {
-		return 0
-	}
-
 	for i := len(deltaArray) - 1; i > 0; i-- {
 		delta := deltaArray[i-1] - deltaArray[i]
 		newDelta = append(newDelta, delta)
 	}
 
 	// Otherwise, keep exterpolating
-	return newDelta[len(newDelta)-1] + extrapolate(newDelta)
+	return newDelta[len(newDelta)-1] - extrapolate(newDelta)
+}
+
+func extrapolateBackwards(input []int) int {
+	var deltaArray []int
+	allZero := true
+	for i := len(input) - 1; i > 0; i-- {
+		delta := input[i] - input[i-1]
+		deltaArray = append(deltaArray, delta)
+		if delta != 0 {
+			allZero = false
+		}
+	}
+
+	if allZero {
+		return 0
+	}
+
+	slices.Reverse(deltaArray)
+
+	fmt.Println(input)
+	// We are not to all zeroes, so keep extrapolating backwards
+	return input[0] - extrapolateBackwards(deltaArray)
 }
 
 func challenge9b(input string) int {
 	answer := 0
 	lines := utils.SplitLines(input)
 	for _, line := range lines {
+		fmt.Println("NEW ENTRY")
+
 		ints := convertToInts(line)
-		inc := ints[len(ints)-1] + extrapolate(ints)
+		//fmt.Printf("input: %v\n", ints)
+		inc := ints[0] - extrapolateBackwards(ints)
 		answer += inc
-		fmt.Println(inc)
 	}
 	return answer
 }
